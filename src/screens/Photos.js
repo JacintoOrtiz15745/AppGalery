bd7723e118844e9162940597fb2cd5b4f6e2afa9
JacintoOrtiz15745 <andresjacinto_araujo@ucol.mx>
2022-01-18 11:58:20 -0600

    Photo list done

--- src/screens/Photos.js
+++ src/screens/Photos.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, Text, ActivityIndicator, FlatList, Image} from 'react-native';
+import {PhotoImage} from '../components/PhotoImage';
 
-const Photos = () => { 
+const Photos = () => {
+  const [photos, setPhotos] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchPhotos = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/photos');
+    const data = await response.json();
+
+    setPhotos(data);
+  };
+
+  useEffect(() => {
+    fetchPhotos();
+  }, []);
 
   return (
     <View>
-      <Text>Photos</Text>
-      <Text>Photos</Text>
-      <Text>Photos</Text>
-      <Text>Photos</Text>
+      <FlatList
+        data={photos}
+        keyExtractor={item => item.id}
+        numColumns={2}
+        renderItem={({item}) => (
+          <Image style={{height: 200, width: '50%'}} source={{uri: item.url}} />
+        )}
+      />
     </View>
   );
 };
